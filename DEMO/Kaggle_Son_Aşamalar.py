38ac7048c4e2aab0101afaf83717009e70d9682c
afa11 <fanarslan@gmail.com>
2023-09-10 12:25:03 +0300

    for kaggle final things

---
+++ DEMO/Kaggle_Son_Aşamalar.py
@@ -0,0 +1,36 @@
+# Kaggle da işimiz kolaylasın diye iki fonksiyon
+#Gerekenler:
+
+#prediction //// model: model nesnesi, param_grid: dictionary, X: train X, y train Y, X_test test X
+
+#save //// test_predictions: output of the prediction function , ids: dataframe["ID"] gibi bir yapı , outputcolumn1name, outputcolumn2name, outputfilename
+
+
+
+
+
+def save(test_predictions, ids , outputcolumn1name, outputcolumn2name, outputfilename):
+  import pandas as pd
+
+  # Create a DataFrame by zipping passenger IDs and predictions
+  result_df = pd.DataFrame(list(zip(ids, test_predictions)), columns=[outputcolumn1name, outputcolumn2name])
+
+  # Save the result to a CSV file
+  result_df.to_csv(outputfilename, index=False)
+
+
+
+def prediction(model, param_grid, X, y, X_test):
+  from sklearn.model_selection import GridSearchCV, StratifiedKFold
+
+
+  grid_search = GridSearchCV(model, param_grid, cv=StratifiedKFold(n_splits=10) , n_jobs=-1)
+
+  grid_search.fit(X, y)
+
+
+  best_model = grid_search.best_estimator_
+  best_params = grid_search.best_params_
+  test_predictions = best_model.predict(X_test)
+
+  return test_predictions
